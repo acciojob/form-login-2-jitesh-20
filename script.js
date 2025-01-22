dd8ff355f8eb6ef238717ed36edf2058347edecd
Jitesh Kumar <105545377+jitesh-20@users.noreply.github.com>
2025-01-22 11:50:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+function displayFormInfo(){
+	let firstName=document.getElementById("firstname").value
+let lastName=document.getElementById("lastname").value
+let phone=document.getElementById("phone").value
+let email=document.getElementById("emailId").value
+	let alertMessage = `First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phone} Email ID: ${email}`;
+	alert(alertMessage)
+}
